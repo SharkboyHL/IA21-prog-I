001913a2923eb61fb95f99a4f628eb1d132b40fe
SharkboyHL <104568918+SharkboyHL@users.noreply.github.com>
2022-07-07 10:03:32 -0300

    Add files via upload

---
+++ Lista 001 - POO/ex_veic/src/ex_veic/Veiculos.java
@@ -0,0 +1,24 @@
+package ex_veic;
+public class Veiculos {
+    
+    private int passageiros;
+    private int combustivel;
+    private int consumo;
+    
+    public Veiculos(int pass, int comb, int cons){
+    
+        this.passageiros = pass;
+        this.combustivel = comb;
+        this.consumo = cons;
+    }
+    
+    public int calcular_consumo(){
+        return(this.combustivel * this.consumo);
+    }
+        
+    public int get_passageiros(){
+        return this.passageiros;
+    }
+}
+
+
